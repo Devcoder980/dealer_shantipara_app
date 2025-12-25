48d786b89a25e25999c0e3dea779e62c85120b43
Devcoder980 <pbind4545@gmail.com>
2025-12-25 18:30:00 +0530

    Add reusable widgets and project assets

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+/// Custom text field with validation support
+class CustomTextField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String? labelText;
+  final String? hintText;
+  final IconData? prefixIcon;
+  final Widget? suffix;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+  final void Function(String)? onSubmitted;
+  final TextInputAction? textInputAction;
+  final int? maxLines;
+  final bool readOnly;
+
+  const CustomTextField({
+    super.key,
+    this.controller,
+    this.labelText,
+    this.hintText,
+    this.prefixIcon,
+    this.suffix,
+    this.obscureText = false,
+    this.keyboardType,
+    this.validator,
+    this.onChanged,
+    this.onSubmitted,
+    this.textInputAction,
+    this.maxLines = 1,
+    this.readOnly = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      validator: validator,
+      onChanged: onChanged,
+      onFieldSubmitted: onSubmitted,
+      textInputAction: textInputAction,
+      maxLines: maxLines,
+      readOnly: readOnly,
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        prefixIcon: prefixIcon != null ? Icon(prefixIcon) : null,
+        suffix: suffix,
+      ),
+    );
+  }
+}
